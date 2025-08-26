6aa4dfa172049dafa31b7448841b070e20e666d1
DerekDLV <derekdlv@localhost.localdomain>
2025-08-26 10:47:09 -0600

    Nav disenno

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
  import { header } from "./components/header/headerComponent.js";
  import { seccion1 } from "./components/seccion1/seccion1Component.js";
- 
+ import { nav } from "./components/nav/navComponent.js";
  function seccion(){
 
     let seccion = document.createElement('section');
@@ -10,7 +10,9 @@
 
     // seccion
     seccion.appendChild(seccion1());
-    
+     
+    // nav
+    seccion.appendChild(nav())
     return seccion;
     
 }
